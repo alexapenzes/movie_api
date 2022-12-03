fe3ba622acadec29d0bf135b9c8695473bef7b50
alexapenzes <penzesalexa@gmail.com>
2022-12-03 07:53:18 +0100

    remove allowedOrigins

--- index.js
+++ index.js
@@ -23,19 +23,7 @@ mongoose.connect( process.env.CONNECTION_URI, {
 });
 
 const app = express();
-
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://ap-myflix.herokuapp.com', 'http://localhost:3000', 'https://myflix-ap.herokuapp.com', 'http://localhost:4200'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 //Middleware to...
 app.use(express.static("public")); // serve static files
